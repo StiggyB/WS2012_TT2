dcfd4b0087e82517bf7524112e4b485757c56b57
StiggyB <benjamin.burchard@gmail.com>
2013-06-17 14:33:35 +0200

    Fixed moving Target for just one active target

--- WS2012_TT2_RL/main/de/haw_hamburg/inf/gui/GUIControl.java
+++ WS2012_TT2_RL/main/de/haw_hamburg/inf/gui/GUIControl.java
@@ -1,12 +1,12 @@
 package de.haw_hamburg.inf.gui;
 
-import java.awt.EventQueue;
 import java.util.Observable;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class GUIControl extends Observable implements Runnable {
 
+    private volatile int targetHopTime = 2;
     private volatile boolean running;
 
     public GUIControl() {
@@ -19,19 +19,22 @@ public class GUIControl extends Observable implements Runnable {
     }
 
     private void moveTarget(int t) {
-        
         notifyObservers(t * 10);
+        long t1 = System.currentTimeMillis();
+        setChanged();
         // gg.togglePanel(t*10);
         System.out.println("T on P" + t * 10 + " toggled");
         try {
 
-            TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(targetHopTime);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        setChanged();
         notifyObservers(t * 10);
+        long t2 = System.currentTimeMillis();
+        setChanged();
+        System.out.println("Time Passed: " + (t2 - t1)/1000 + "s");
         // gg.togglePanel(t*10);
     }
 
@@ -41,5 +44,21 @@ public class GUIControl extends Observable implements Runnable {
         while (running) {
             moveTarget(r.nextInt(5) + 1);
         }
+    }
+    
+    public void reset() {
+        running = true;
+    }
+    
+    public void terminate() {
+        running = false;
+    }
+
+    public int getTargetHopTime() {
+        return targetHopTime;
+    }
+
+    public void setTargetHopTime(int targetHopTime) {
+        this.targetHopTime = targetHopTime;
     }
 }
